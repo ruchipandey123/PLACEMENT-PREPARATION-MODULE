781e508364f39e59f673b711aecb226090004488
RUCHI PANDEY <68181236+ruchipandey123@users.noreply.github.com>
2022-07-08 10:25:08 +0530

    Time: 9 ms (41.93%), Space: 9.8 MB (83.47%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+       vector<int> ans=prices;
+        for(int i=0;i<prices.size();i++)
+        {
+            for(int j=i+1;j<prices.size();j++)
+            {
+                if(prices[j]<=prices[i] && i<j)
+                {
+                    ans[i]=prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
